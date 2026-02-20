7eca59afddd306de104a45b0c8921d90bc9776e0
Jeet Ghosh <ghoshjeet455@gmail.com>
2026-02-20 22:17:09 +0530

    user model created

---
+++ model/userModel.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: string,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: string,
+      required: true,
+    },
+    bio: {
+      type: string,
+      maxlength: 160,
+      default: "",
+    },
+    avatar: {
+      type: string,
+      default: "",
+    },
+    coverImage: {
+      type: string,
+      default: "",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    blockUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
