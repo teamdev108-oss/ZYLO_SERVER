4e091c701e2978462474ad2db4529653b3d3a73b
Souvik Sarkar <sarkorsouvo@gmail.com>
2026-02-20 19:07:27 +0530

    db connection established

--- config/db.js
+++ config/db.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const connectDb = async () => {
   try {
     let connection = await mongoose.connect(process.env.MONGODB_URL);
-    if (connection.ok) console.log("db connected");
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     process.exit(1);
   }
 };
+export default connectDb;
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+import  express from 'express'
+import  {config} from 'dotenv'
+import  connectDb  from "./config/db.js"
+config()
+const app =  express()
+const port = process.env.PORT
 
+connectDb().then(()=>{
+    app.listen(port,()=>{
+        console.log(`app running port ${port}`);
+        
+    })
+})
+
+
+ 
