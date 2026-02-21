1197cb67628b34b4c217468ce5fc247e5aeec3b7
Jeet Ghosh <ghoshjeet455@gmail.com>
2026-02-21 12:49:29 +0530

    errorHandler added

--- server.js
+++ server.js
@@ -4,12 +4,30 @@ import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.js";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xssClean from "xss-clean";
+import rateLimit from "express-rate-limit";
+
 config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "20kb" }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xssClean());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Running 🚀" });
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use("/api", limiter);
 
 app.use(
   cors({
@@ -17,7 +35,6 @@ app.use(
     credentials: true,
   }),
 );
-
 
 app.use(errorHandler);
 connectDb().then(() => {
