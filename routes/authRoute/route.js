276445166268b43c184115e9659f0da1a1804889
Jeet Ghosh <ghoshjeet455@gmail.com>
2026-02-23 14:14:11 +0530

    minor change

--- routes/authRoute/route.js
+++ routes/authRoute/route.js
@@ -3,12 +3,14 @@ import {
   createUser,
   login,
   logoutUser,
+  verifyEmail,
 } from "../../controllers/user/auth/user.controller.js";
 
 const route = Router();
 
 route.post("/register/user", createUser);
 route.post("/login/user", login);
-route.post("logout/user",logoutUser)
+route.post("/logout/user", logoutUser);
+route.post("/verify/user", verifyEmail);
 
 export default route;
